028c0aa351543fab5fcb758e4d0d5de1a1921bba
Darsonn <admin@darsonn.pl>
2023-09-10 23:46:23 +0200

    added sendLinkMessage

--- src/main/java/pl/darsonn/discordbot/commands/CommandsCreator.java
+++ src/main/java/pl/darsonn/discordbot/commands/CommandsCreator.java
@@ -1,16 +1,11 @@
 package pl.darsonn.discordbot.commands;
 
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.interactions.commands.Command;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
 import static net.dv8tion.jda.api.interactions.commands.OptionType.BOOLEAN;
-import static net.dv8tion.jda.api.interactions.commands.OptionType.STRING;
 
 public class CommandsCreator {
     public void createCommands(JDA builder) {
@@ -39,6 +34,12 @@ public class CommandsCreator {
                         .addOption(BOOLEAN, "administrator", "Status of role Administrator", true)
                         .addOption(BOOLEAN, "developer", "Status of role Developer", true)
                         .addOption(BOOLEAN, "creator", "Status of role Creator", true)
+                        .setGuildOnly(true)
+                        .setDefaultPermissions(DefaultMemberPermissions.DISABLED)
+        );
+
+        commands.addCommands(
+                Commands.slash("sendlinkmessage", "Wysyła wiadomość z przydatnymi linkami")
                         .setGuildOnly(true)
                         .setDefaultPermissions(DefaultMemberPermissions.DISABLED)
         );
