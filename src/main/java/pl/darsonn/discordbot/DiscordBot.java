cd0653e8ce1aee964f94c1f8bf62ec51ba79142c
Darsonn <87154431+Darsonn@users.noreply.github.com>
2023-11-12 19:54:47 +0100

    :coffin: removed unused part of code

--- src/main/java/pl/darsonn/discordbot/DiscordBot.java
+++ src/main/java/pl/darsonn/discordbot/DiscordBot.java
@@ -1,19 +1,14 @@
 package pl.darsonn.discordbot;
 
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import pl.darsonn.Main;
 import pl.darsonn.discordbot.commands.CommandsCreator;
 import pl.darsonn.discordbot.commands.EventHandler;
 
-import java.awt.*;
-import java.time.Instant;
 import java.util.EnumSet;
-import java.util.Objects;
 
 public class DiscordBot {
     public void startBot(String token) {
